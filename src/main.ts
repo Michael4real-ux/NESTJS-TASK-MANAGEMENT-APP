4fdf8540a4497d4776f57672cc75689ca67f9266
Damilola Babalola <dammydeji.dd@gmail.com>
2023-04-23 09:40:45 +0100

    Cors refactoring

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get<{ [key: string]: number }>('server');
+  const serverConfig = config.get<any>('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  if (serverConfig.NODE_ENV) {
+  if (process.env.NODE_ENV === 'development') {
     app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting request from origin ${serverConfig.origin}`);
   }
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
